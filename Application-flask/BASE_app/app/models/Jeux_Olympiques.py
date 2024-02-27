7662049ee96e515472bd5f0dfea31fd683ea5652
Maxime-Griveau <griveaumaxime@gmail.com>
2024-02-27 14:57:32 +0100

    fin de l'instanciation de la base : reste à la documenter

--- Application-flask/BASE_app/app/models/Jeux_Olympiques.py
+++ Application-flask/BASE_app/app/models/Jeux_Olympiques.py
@@ -6,26 +6,28 @@ from ..app import app, db
 class Donnees(db.Model):
     __tablename__ = "donnees"
 
-    id = db.Column(db.String(45), primary_key=True, nullable = False, unique=True)
+    id_team = db.Column(db.String(45), primary_key=True, nullable = False, unique=True)
     population = db.Column(db.Integer)
     richesse = db.Column(db.Float)
     investissement = db.Column(db.Float)
 
     # propriétés de relation (à ajouter)
-    areas = db.relationship('Area',  backref='areas',  lazy=True)
-    
 
+    formulaires = db.relationship('Formulaire', backref='donnees', lazy=True)
+    medailles = db.relationship('Medailles', backref='donnees', lazy=True)
+    
     def __repr__(self):
         return '<Donnees %r>' % (self.id) 
 
 class Formulaire(db.Model):
     __tablename__ = "Formulaire"
 
-    id = db.Column(db.String(45))
+    donnees_id = db.Column(db.String(45), db.ForeignKey('donnees.id'))
     noc = db.Column(db.String(3))
     year = db.Column(db.DateTime)
     
-    # propriétés de relation (à ajouter)
+   
+    
 
     def __repr__(self):
         return '<Formulaire %r>' % (self.noc) 
@@ -33,14 +35,13 @@ class Formulaire(db.Model):
 class Medailles(db.Model):
     __tablename__ = "medailles"
 
-    id = db.Column(db.String(45)) 
+    donnees_id = db.Column(db.String(45), db.ForeignKey('donnees.id'))
     gold = db.Column(db.Integer)
     silver = db.Column(db.Integer)
     bronze= db.Column(db.Integer)
     total = db.Column(db.Integer)
 
-    # propriétés de relation (à ajouter)
-
+    
     def __repr__(self):
         return '<Boundaries %r>' % (self.total) 
 

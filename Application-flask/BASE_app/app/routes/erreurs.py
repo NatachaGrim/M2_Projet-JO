19141b312fffcb26a8bb3fa591476f10d44dfb7a
Natacha Grim <natacha.grim@chartes.psl.eu>
2024-03-03 12:57:55 +0100

    docu routes erreurs

--- Application-flask/BASE_app/app/routes/erreurs.py
+++ Application-flask/BASE_app/app/routes/erreurs.py
@@ -3,10 +3,22 @@ from flask import render_template
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+	""" Gestion des erreurs 404
+	
+	:param error: Objet d'erreur
+	:type error: Exception
+	:returns: Page HTML correspondant au type d'erreur 404
+	"""
+	return render_template('404.html'), 404
 
 @app.errorhandler(500)
 @app.errorhandler(503)
 def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
+	""" Gestion des erreurs de serveur type 500 incluant un rollback sur la base de données.
+	
+	:param error: Objet d'erreur
+	:type error: Exception
+	:returns: Page HTML correspondant au type d'erreur 500
+	"""
+	db.session.rollback()
+	return render_template('500.html'), 500

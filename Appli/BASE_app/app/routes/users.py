a20a3096115d9133c519365e04805edaa2b99feb
TheoBurnel <theo.burnel@gmail.com>
2024-03-06 19:21:51 +0100

    boostrap 1

---
+++ Appli/BASE_app/app/routes/users.py
@@ -0,0 +1,100 @@
+from ..app import app, db, login
+from flask import render_template, request, flash, abort, redirect, url_for, abort 
+from ..models.Jeux_Olympiques import Donnees, Formulaire, Medailles, Pays
+from ..models.formulaires import InsertionUsers, Connexion
+from ..utils.transformations import clean_arg
+from ..models.users import Users
+from flask_login import current_user, logout_user, login_user
+from functools import wraps
+from flask import abort
+
+
+@app.route("/connexion", methods=['GET', 'POST'])
+@app.route("/connexion/<int:page>", methods=['GET', 'POST'])
+def connexion(page=1):
+    """
+    Route pour gérer la connexion des utilisateurs. 
+
+    Parameters
+    ----------
+    page : int, optional
+        Numéro de la page actuelle, par défaut à 1.
+
+    Returns
+    -------
+    template
+        Retourne le template de la page de connexion avec les données nécessaires.
+    """
+    form = Connexion()
+    utilisateur = ""
+    try:
+        donnees = [] # Initialiser données comme une liste vide
+        administrateur = None #Administrateur comme None
+        if form.validate_on_submit():
+            mot_de_passe = clean_arg(request.form.get("mot_de_passe", None))
+            mail = clean_arg(request.form.get("mail", None))
+            if current_user.is_authenticated is False:
+                if mail and mot_de_passe: 
+                    result = Users.Identification(password=mot_de_passe, mail=mail)
+                    if result:
+                        #is instance vérifie qu'un object est instance d'une classe
+                        if isinstance(result, tuple): # Vérifie si le résultat est un tuple (dans ma méthode() Identification, si l'utilisateur est administrateur je renvoie un tuple)
+                            utilisateur, administrateur = result # Déballe le tuple
+
+                        else:
+                            utilisateur = result # L'utilisateur n'est pas administrateur
+                            administrateur = None
+                            print("utilisateur")
+                        if administrateur:
+                            login_user(utilisateur)
+                            flash(f"{utilisateur.mail} est désormais connecté en tant qu'administrateur.", 'success')
+                            
+
+                        else:
+                            login_user(utilisateur)
+                            flash(f"{utilisateur.mail} est désormais connecté en tant que journaliste.", 'success')
+                            
+
+                    else:
+                        flash("Adresse mail ou mot de passe incorrect.", 'error')
+                else:   
+                    flash("Impossible de vous connecter, merci de fournir vos informations de connexion.", 'error')
+            else:
+                flash("Vous êtes déjà connecté.", 'info')
+
+    except Exception as e:
+        print("Une erreur est survenue : " + str(e))
+        flash("Une erreur s'est produite lors de la connexion, avez-vous respecté les contraintes de saisie ?" + str(e), 'info')
+        db.session.rollback()
+    
+    return render_template("pages/connexion.html", sous_titre="Recherche", donnees=donnees, form=form, utilisateur=utilisateur, administrateur=administrateur)
+
+
+
+
+def admin_required(f): #défini un décorateur "admin_requiered" qui prend une fonction 'f' comme argiment
+    @wraps(f) #Ce décorateur permet de s'assurer que les métadonnées de la fonction originales soient inclues dans le décorateur
+    def decorated_function(): #décorateur
+        if not current_user.administrateur: #Si l'utilisateur n'est pas administrateur 
+            abort(403)  # Interdit (renvoie une erreur 403)
+        return f() #sinon tout va bien 
+    return decorated_function
+
+
+
+@app.route("/utilisateurs/deconnexion", methods=["POST", "GET"])
+def deconnexion():
+    """
+    Route pour gérer la déconnexion des utilisateurs.
+
+    Ne retourne rien 
+    -------
+    redirect
+        Redirige vers la page d'accueil après la déconnexion.
+    """
+    if current_user.is_authenticated is True:
+        logout_user()
+    flash("Vous êtes déconnecté", "info")
+    return redirect(url_for("donnees"))
+
+login.login_view = "connexion"
---
+++ Appli/README.md
@@ -0,0 +1,130 @@
+# Application flask
+Notre application permet de consulter les résultats de différents pays à diverses éditions des Jeux Olympiques.
+
+## Contributeurs
+Les personnes ayant contribué sont :
+- [Théo Burnel](https://github.com/TheoBurnel) ;
+- [Natacha Grim](https://github.com/NatachaGrim) ;
+- [Maxime Griveau](https://github.com/Maxime-Griveau) ;
+- [Mohammed Mechentel]().
+
+## Fonctionnalités
+Notre application dispose des fonctionnalités suivantes :
+
+## Variables
+Les variables sont indispensables au bon fonctionnement de l'application. Vous devrez les ajouter manuellement dans un fichier. Copiez-collez simplement ce bloc de texte :
+
+```text
+DEBUG=False
+SQLALCHEMY_DATABASE_URI= [URI DE LA BASE SQLITE]
+PAYS_PER_PAGE = 10 
+RESOURCES_PER_PAGE = 20
+SQLALCHEMY_ECHO=False
+WTF_CSRF_ENABLE=True
+SECRET_KEY=[LA CLÉ SERA ENVOYÉE PAR MAIL]
+```
+
+## Instructions : un premier lancement
+
+### Étape 1 : installer Python
+- Ouvrez votre terminal, collez-y la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+sudo apt install python3
+```
+
+### Étape 2 : cloner l'application
+- Dans le terminal, collez la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+git clone https://github.com/NatachaGrim/M2_Projet-JO.git
+```
+
+### Étape 3 : installer un environnement virtuel
+- Dans le terminal, collez la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+cd Application
+```
+
+- Puis collez-y la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+virtualenv env -p python3
+```
+
+### Étape 4 : télécharger la base de données
+- Téléchargez la base de données et déplacez-la dans le dossier "Application".
+
+### Étape 5 : saisir les variables
+- Dans le terminal, collez la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+touch .env
+```
+
+- Ouvrez le fichier ```.env``` et copiez-collez-y ce bloc de code avec ```ctrl+v``` :
+
+```
+```
+
+- Enregistrez avec ```ctrl+s``` et fermez le fichier.
+
+## Étape 6 : installer les dépendances
+- Dans le terminal, collez la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+source env/bin/activate
+```
+
+- Collez ensuite la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+pip install -r requirements.txt
+```
+
+## Étape 7 : lancer l'application
+
+- Dans le terminal, collez la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+python run.py
+```
+
+L'application devrait démarrer. Ouvrez ensuite votre navigateur web et saisissez le nom d'une route. Par exemple :
+
+```shell
+localhost:5000/nomDeRoute
+```
+
+Nos différentes routes sont consultables dans [ce dossier]().
+
+## Étape 8 : quitter l'application
+- Dans le terminal, maintenez la touche ```ctrl``` enfoncée et appuyez sur la touche ```c``` ;
+- Collez-y ensuite la ligne suivante avec ```ctrl+maj+v``` et appuyez sur la touche ```Entrée``` :
+
+```shell
+deactivate
+```
+
+## Instructions : mémo
+
+Pour lancer l'application :
+```shell
+cd cheminJusqu'auDossierApplication
+```
+
+```shell
+source env/bin/activate
+```
+
+```shell
+python run.py
+```
+
+Pour quitter l'application :
+- Maintenir ```ctrl``` et appuyer sur ```c``` ;
+
+```shell
+deactivate
+```

d72f699867f7257bf58e4dd3fc3ce420dbd870d2
Natacha Grim <natacha.grim@chartes.psl.eu>
2024-03-10 18:28:01 +0100

    Ajout et documentation des erreurs

--- Application-flask/BASE_app/app/routes/erreurs.py
+++ Application-flask/BASE_app/app/routes/erreurs.py
@@ -3,22 +3,55 @@ from flask import render_template
 
 @app.errorhandler(404)
 def not_found_error(error):
-	""" Gestion des erreurs 404
+	"""
+	Route permettant de gérer les erreurs 404 (ressource non trouvée)
 	
-	:param error: Objet d'erreur
-	:type error: Exception
-	:returns: Page HTML correspondant au type d'erreur 404
+	Parameters
+	----------
+	error : exception
+		L'objet correspondant à l'erreur
+	
+	Returns
+	-------
+	template
+		Retourne le template '404.html' ainsi que le code d'erreur 404
 	"""
 	return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def forbidden_error(error):
+	"""
+	Route permettant de gérer les erreurs 403 (accès interdit)
+	
+	Parameters
+	----------
+	error : exception
+		L'objet correspondant à l'erreur
+	
+	Returns
+	-------
+	template
+		Retourne le template '403.html' ainsi que le code d'erreur 403
+	"""
+	return render_template('403.html'), 403
 
 @app.errorhandler(500)
 @app.errorhandler(503)
 def internal_error(error):
-	""" Gestion des erreurs de serveur type 500 incluant un rollback sur la base de données.
+	"""
+	Route permettant de gérer les erreurs de type 500
+		500 : erreur interne de serveur
+		503 : service indisponible
 	
-	:param error: Objet d'erreur
-	:type error: Exception
-	:returns: Page HTML correspondant au type d'erreur 500
+	Parameters
+	----------
+	error : exception
+		L'objet correspondant à l'erreur
+	
+	Returns
+	-------
+	template
+		Retourne le template '500.html' ainsi que le code d'erreur 500
 	"""
 	db.session.rollback()
 	return render_template('500.html'), 500
